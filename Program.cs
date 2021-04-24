5a6a2de808ab53fdade4e073913f415d3e466901
MINWINPC\UserPC <Nattatida.ek1111@mail.kmutt.ac.th>
2021-04-24 15:10:52 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            switch(input)
+                
+            {
+
+
+            int day = Console.Write(monday == 1 | tuesday == 2 | Wednesday == 3 | Thursday == 4 | Friday == 5 | Saturday == 6 | sunday == 7);
+            int time = int.Parse(Console.ReadLine());
+                int breakfastset = 5;
+                int WeekendSet = 5;
+                int Coffee = 3;
+                int whatmenu
+
+
+             
+                
+                
+                case day;
+       
+                    Console.WriteLine("what do you want to eat" );
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                    whatmenu = int.Parse(Console.ReadLine());
+
+                    if (whatmenu == breakfastset) ;
+                    
+            {
+                if (time < 8 && time > 11) ;
+                Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                whatmenu == breakfastset remove 1;
+                Console.WriteLine(breakfastset == 4 | WeekendSet == 5 | Coffee == 3);
+                whatmenu == breakfastset remove 1;
+                Console.WriteLine(breakfastset == 3 | WeekendSet == 5 | Coffee == 3);
+                whatmenu == breakfastset > remove 1;
+                Console.WriteLine(breakfastset == 2 | WeekendSet == 5 | Coffee == 3);
+                whatmenu == breakfastset > remove 1;
+                Console.WriteLine(breakfastset == 1 | WeekendSet == 5 | Coffee == 3);
+                whatmenu == breakfastset > remove 1;
+                Console.WriteLine(breakfastset == 0 | WeekendSet == 5 | Coffee == 3);
+                
+            }
+            
+
+            {
+                Console.WriteLine("what do you want to eat");
+                Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                whatmenu = int.Parse(Console.ReadLine());
+                if (whatmenu == WeekendSet) ;
+                {
+                    if (time < 8 && time > 18) ;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                    whatmenu == WeekendSet remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 4 | Coffee == 3);
+                    whatmenu == WeekendSet remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 3 | Coffee == 3);
+                    whatmenu == WeekendSet remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 2 | Coffee == 3);
+                    whatmenu == WeekendSet remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 1 | Coffee == 3);
+                    whatmenu == WeekendSet remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                 
+                }
+            }
+            {
+                Console.WriteLine("what do you want to eat");
+                Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                whatmenu = int.Parse(Console.ReadLine());
+                if (whatmenu == Coffee) ;
+
+                {
+                    if (time < 8 && time > 18) ;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 3);
+                    whatmenu == Coffee remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 2);
+                    whatmenu == Coffee remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 1);
+                    whatmenu == Coffee remove 1;
+                    Console.WriteLine(breakfastset == 5 | WeekendSet == 5 | Coffee == 0);
+                    
+
+                }
+                {
+                    if (day > 1 && day 5);
+                    Console.WriteLine ("buy weekendset")
+                        whatmenu == WeekendSet == error;
+
+                }
+
+                default:
+                    Console.WriteLine("");
+                Console.WriteLine("Please enter a valid menu");
+                Console.WriteLine("plaase enetr menu again");
+                 
+
+
+
+            }
+              
+
+            }
+            
+
+
+            
+
+            
+        }
+    }
+}
